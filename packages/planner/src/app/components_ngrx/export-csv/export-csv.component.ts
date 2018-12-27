8ae0f3f7bbc4baa3772ddd959d7d7bf9cc12a2ad
sahil143 <sahilbudhwar143@gmail.com>
2018-12-27 20:57:53 +0530

    feat(export-csv): create a component to export csv files

---
+++ packages/planner/src/app/components_ngrx/export-csv/export-csv.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, Inject } from '@angular/core';
+import { FilterService } from '../../services/filter.service';
+import { WIT_API_URL, Space } from 'ngx-fabric8-wit';
+import { SpaceQuery } from '../../models/space';
+import { Observable, of, combineLatest } from 'rxjs';
+import { filter, switchMap } from 'rxjs/operators';
+import { EQUAL, AND } from '../../services/query-keys';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'export-csv',
+  templateUrl: './export-csv.component.html',
+})
+export class ExportCsvComponent {
+  @Input() isDisabled: boolean;
+  @Input() query: string;
+
+  public csvLink: Observable<string> = combineLatest(
+    this.spaceQuery.getCurrentSpace.pipe(filter((s) => !!s)),
+    this.route.queryParams.pipe(filter((q) => !!Object.keys(q).length)),
+  ).pipe(
+    switchMap(([space, query]) => {
+      console.log('################333333', query);
+      const currentQuery = this.filterService.queryToJson(query.q);
+      const spaceQuery = this.filterService.queryBuilder('space', EQUAL, space.id);
+      const expression = {
+        expression: this.filterService.queryJoiner(currentQuery, AND, spaceQuery),
+      };
+      return of(
+        this.baseApiURL +
+          'search/workitems/csv?' +
+          Object.keys(expression)
+            .map((k) => 'filter[' + k + ']=' + encodeURIComponent(JSON.stringify(expression[k])))
+            .join('&'),
+      );
+    }),
+  );
+
+  constructor(
+    private spaceQuery: SpaceQuery,
+    private filterService: FilterService,
+    private route: ActivatedRoute,
+    @Inject(WIT_API_URL) private baseApiURL: string,
+  ) {}
+}
